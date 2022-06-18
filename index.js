2081a22b32cce0e2324e8cc86268ca39c93c0905
Chloe Cartron <chloe.cartron@gmail.com>
2022-06-18 17:15:52 +0200

    api retrieve data from various websites

--- index.js
+++ index.js
@@ -6,27 +6,80 @@ const { ppid } = require('process')
 
 const app = express()
 
+const newspapers = [
+  {
+    name: 'techcrunch',
+    address: "https://techcrunch.com/"
+  },
+  {
+    name: 'adexchanger',
+    address: 'https://www.adexchanger.com/'
+  },
+  {
+    name: 'venturebeat',
+    address: 'https://venturebeat.com/'
+  },
+  {
+    name: 'exchangewire',
+    address: 'https://www.exchangewire.com/'
+  }
+]
+
 const articles = []
+
+newspapers.forEach(newspaper => {
+  axios.get(newspaper.address)
+        .then(response => {
+          const html = response.data
+          const $ = cheerio.load(html)
+
+          $('a:contains("mobile")', html).each(function () {
+            const title = $(this).text()
+            const url = $(this).attr('href')
+
+            articles.push({
+              title,
+              url,
+              source: newspaper.name
+            })
+          })
+        })
+})
 
 app.get('/', (req, res) => {
   res.json('Welcome to my mobile tech news API')
 })
 
 app.get('/news', (req, res) => {
-  axios.get('https://techcrunch.com/')
-       .then((response) => {
-         const html = response.data
-         const $ = cheerio.load(html)
+  res.json(articles)
+})
 
-         $('a:contains("mobile")', html).each(function () {
-           const title = $(this).text()
-           const url = $(this).attr('href')
-            articles.push({
+app.get('/news/:newspaperId', async (req, res) => {
+  const newspaperId = req.params.newspaperId
+
+  const newspaperAddress = newspapers.filter(newspaper => newspaper.name === newspaperId)[0].address
+
+  console.log(newspaperAddress)
+
+  axios.get(newspaperAddress)
+        .then(response => {
+          const html = response.data
+          const $ = cheerio.load(html)
+
+          const specificArticles = []
+
+          $('a:contains("mobile")', html).each(function () {
+            const title = $(this).text()
+            const url = $(this).attr('href')
+
+            specificArticles.push({
               title,
-              url
+              url,
+              source: newspaperId
             })
           })
-         res.json(articles)
-  }).catch((err) => console.log(err))
+          res.json(specificArticles)
+        }).catch(err => console.log(err))
 })
+
 app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`))

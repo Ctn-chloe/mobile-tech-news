f01d598dd3397d272068f638cabce5baa7a135fe
Chloe Cartron <chloe.cartron@gmail.com>
2022-06-18 16:43:20 +0200

    retrive all title and url from article matching keyword mobile from one website

--- index.js
+++ index.js
@@ -6,8 +6,27 @@ const { ppid } = require('process')
 
 const app = express()
 
+const articles = []
+
 app.get('/', (req, res) => {
   res.json('Welcome to my mobile tech news API')
 })
 
+app.get('/news', (req, res) => {
+  axios.get('https://techcrunch.com/')
+       .then((response) => {
+         const html = response.data
+         const $ = cheerio.load(html)
+
+         $('a:contains("mobile")', html).each(function () {
+           const title = $(this).text()
+           const url = $(this).attr('href')
+            articles.push({
+              title,
+              url
+            })
+          })
+         res.json(articles)
+  }).catch((err) => console.log(err))
+})
 app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`))
